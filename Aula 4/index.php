fd38763b7761e97edac95f07f64542ecf785b32c
Diego Oliveira Ramos <oliver.diegoramos@gmail.com>
2024-09-17 15:15:43 -0300

    Aula 4 - Filtro

---
+++ Aula 4/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main class="container">
+        <h1>Top vendas</h1>
+        <form action="verificar.php" method="post" class="container-input">
+            <input type="text" name="pesquisa" id="pesquisa">
+            <button type="submit">🔍</button>
+        </form>
+        <table>
+            <tr>
+                <th>nome</th>
+                <th>marca</th>
+                <th>quantidade</th>
+                <th>valor</th>
+            </tr>
+            <!-- CODIGO PHP -->
+        </table>
+    </main>
+</body>
+</html>
